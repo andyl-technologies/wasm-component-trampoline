005d111390f6bfd1f67278af1bf9beea5f9604e4
Dylan Plecki <dylan@plecki.net>
2025-05-22 12:10:12 -0700

    Add test runner, will move testing to nix

---
+++ tests/runner/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // TODO(dplecki): Accept CLI arguments for WASM files to load into the crate's composition
+    //  graph, then execute a well-known entrypoint within the "main" package specified in the
+    //  arguments.
+    todo!()
+}
