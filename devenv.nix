2b4ec6883bd6bed65e060f7d38890379a61bb2b8
Dylan Plecki <dylan@plecki.net>
2025-05-23 12:15:12 -0700

    Add Github test runner

--- devenv.nix
+++ devenv.nix
@@ -1,14 +1,20 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+    # https://devenv.sh/packages/
+    packages = [ pkgs.git ];
 
-  # https://devenv.sh/languages/
-  languages.rust.enable = true;
+    # https://devenv.sh/languages/
+    languages.rust.enable = true;
 
-  # https://devenv.sh/processes/
-  processes.cargo-watch.exec = "cargo-watch";
+    # https://devenv.sh/processes/
+    processes.cargo-watch.exec = "cargo-watch";
 
-  # See full reference at https://devenv.sh/reference/options/
+    # https://devenv.sh/tests/
+    enterTest = ''
+        cargo test
+        cargo fmt --check
+    '';
+
+    # See full reference at https://devenv.sh/reference/options/
 }
