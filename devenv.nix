8e8f20898398dbd907873042d8979c95a8840489
bill fumerola <bill@andyl.com>
2025-05-26 16:58:25 -0700

    chore(devenv): format nix file (#13)
    * chore(devenv): format nix file
    * feat(devenv): git hooks for nix fmt

--- devenv.nix
+++ devenv.nix
@@ -1,20 +1,23 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  ...
+}:
 
 {
-    # https://devenv.sh/packages/
-    packages = [ pkgs.git ];
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
 
-    # https://devenv.sh/languages/
-    languages.rust.enable = true;
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
 
-    # https://devenv.sh/processes/
-    processes.cargo-watch.exec = "cargo-watch";
+  # https://devenv.sh/processes/
+  processes.cargo-watch.exec = "cargo-watch";
 
-    # https://devenv.sh/tests/
-    enterTest = ''
-        cargo test
-        cargo fmt --check
-    '';
+  # https://devenv.sh/tests/
+  enterTest = ''
+    cargo test
+    cargo fmt --check
+  '';
 
-    # See full reference at https://devenv.sh/reference/options/
+  git-hooks.hooks.nixfmt-rfc-style.enable = true;
 }
