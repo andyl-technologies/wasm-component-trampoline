c5ecf0cabf7ae2b06ed7332080c8978ed9424fcc
bill fumerola <bill@andyl.com>
2026-01-07 18:00:40 -0800

    update wasmtime,wit-bindgen

--- src/graph.rs
+++ src/graph.rs
@@ -596,7 +596,7 @@ impl<D: 'static, C: Clone + Send + Sync + 'static> InstanceShadower<D, C> for Sy
                 let fn_trampoline = trampoline.clone();
 
                 instance
-                    .func_new(export_name, move |store, arguments, result| {
+                    .func_new(export_name, move |store, _ty, arguments, result| {
                         let mut result = fn_trampoline.bounce(
                             &shadow_func,
                             store,
@@ -645,7 +645,7 @@ impl<D: Send + 'static, C: Clone + Send + Sync + 'static> InstanceShadower<D, C>
                 let fn_trampoline = trampoline.clone();
 
                 instance
-                    .func_new(export_name, move |store, arguments, result| {
+                    .func_new(export_name, move |store, _ty, arguments, result| {
                         let mut result = fn_trampoline.bounce(
                             &shadow_func,
                             store,
@@ -667,7 +667,7 @@ impl<D: Send + 'static, C: Clone + Send + Sync + 'static> InstanceShadower<D, C>
                 let fn_trampoline = trampoline.clone();
 
                 instance
-                    .func_new_async(export_name, move |store, arguments, result| {
+                    .func_new_async(export_name, move |store, _ty, arguments, result| {
                         let export_name = fn_export_name.clone();
                         let trampoline = fn_trampoline.clone();
                         let interface_path = fn_interface_path.clone();
